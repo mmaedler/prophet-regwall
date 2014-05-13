9f7456a27f4a872b5a0d4fd89dda6ce20a032ffd
mmaedler <mail@moritz-maedler.de>
2014-05-13 16:58:07 +0200

    Initial Commit

---
+++ prophet-regwall.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name: Simple Pull Quote
+ * Plugin URI: http://github.com/mmaedler/prophet-regwall
+ * Description: Adds a register form in front of selected posts
+ * Author: Moritz Mädler
+ * Version: 0.1
+ * Author URI: http://prophet.com
+*/
